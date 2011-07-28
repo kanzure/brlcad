kanzure/brlcad

commit f211650061fad0375a78510eb33872c0e822bacf

quell a slew of verbose strict compilation warnings, shadow warnings, unused params, floating point comparisons
git-svn-id: svn://svn.code.sf.net/p/brlcad/code/brlcad/trunk@36723 2f96ce8b-6d43-0410-b8df-bffccc660ffb

src/librt/prep.c
@@ -1457,10 +1457,14 @@ static int
 unprep_reg_start(struct db_tree_state *tsp,
 		 struct db_full_path *pathp,
 		 const struct rt_comb_internal *comb,
-		 genptr_t client_data)
+		 genptr_t client_data __attribute__((unused)))
 {
-    RT_CK_RTI(tsp->ts_rtip);
-    RT_CK_RESOURCE(tsp->ts_resp);
+    if (tsp) {
+	RT_CK_RTI(tsp->ts_rtip);
+	RT_CK_RESOURCE(tsp->ts_resp);
+    }
+    if (pathp) RT_CK_FULL_PATH(pathp);
+    if (comb) RT_CK_COMB(comb);
 
     /* Ignore "air" regions unless wanted */
     if (tsp->ts_rtip->useair == 0 &&  tsp->ts_aircode != 0) {
@@ -1474,8 +1478,15 @@ static union tree *
 unprep_reg_end(struct db_tree_state *tsp,
 	       struct db_full_path *pathp,
 	       union tree *tree,
-	       genptr_t client_data)
+	       genptr_t client_data __attribute__((unused)))
 {
+    if (tsp) {
+	RT_CK_RTI(tsp->ts_rtip);
+	RT_CK_RESOURCE(tsp->ts_resp);
+    }
+    if (pathp) RT_CK_FULL_PATH(pathp);
+    if (tree) RT_CK_TREE(tree);
+
     return((union tree *)NULL);
 }
 
@@ -1483,7 +1494,7 @@ static union tree *
 unprep_leaf(struct db_tree_state *tsp,
 	    struct db_full_path *pathp,
 	    struct rt_db_internal *ip,
-	    genptr_t client_data)
+	    genptr_t client_data __attribute__((unused)))
 {
     register struct soltab *stp;
     struct directory *dp;
@@ -1773,7 +1784,6 @@ rt_reprep(struct rt_i *rtip, struct rt_reprep_obj_list *objs, struct resource *r
     struct region *rp;
     struct soltab *stp;
     fastf_t old_min[3], old_max[3];
-    int model_extremes_have_changed=0;
     long bitno;
 
     VMOVE(old_min, rtip->mdl_min);
@@ -1855,18 +1865,9 @@ rt_reprep(struct rt_i *rtip, struct rt_reprep_obj_list *objs, struct resource *r
 
     bu_ptbl_free(&rtip->rti_new_solids);
 
-    for (i=0; i<3; i++) {
-	if (rtip->mdl_min[i] != old_min[i]) {
-	    model_extremes_have_changed = 1;
-	    break;
-	}
-	if (rtip->mdl_max[i] != old_max[i]) {
-	    model_extremes_have_changed = 1;
-	    break;
-	}
-    }
-
-    if (model_extremes_have_changed) {
+    if (!VAPPROXEQUAL(rtip->mdl_min, old_min, SMALL_FASTF)
+	|| !VAPPROXEQUAL(rtip->mdl_max, old_min, SMALL_FASTF))
+    {
 	/* fill out BSP, it must completely fill the model BB */
 	fastf_t bb[6];
 

src/librt/db_comb.c
@@ -67,20 +67,29 @@ db_comb_mat_categorize(const fastf_t *matp)
 
     if (!matp) return 0;
 
-    if (matp[0] != 1.0 || matp[5] != 1.0 || matp[10] != 1.0)
+    if (!NEAR_ZERO(matp[0] - 1.0, SMALL_FASTF)
+	|| !NEAR_ZERO(matp[5] - 1.0, SMALL_FASTF)
+	|| !NEAR_ZERO(matp[10] - 1.0, SMALL_FASTF))
+    {
 	status |= STAT_ROT;
+    }
 
-    if (matp[MDX] != 0.0 ||
-	matp[MDY] != 0.0 ||
-	matp[MDZ] != 0.0)
+    if (!NEAR_ZERO(matp[MDX], SMALL_FASTF)
+	|| !NEAR_ZERO(matp[MDY], SMALL_FASTF)
+	|| !NEAR_ZERO(matp[MDZ], SMALL_FASTF))
+    {
 	status |= STAT_XLATE;
+    }
 
-    if (matp[12] != 0.0 ||
-	matp[13] != 0.0 ||
-	matp[14] != 0.0)
+    if (!NEAR_ZERO(matp[12], SMALL_FASTF)
+	|| !NEAR_ZERO(matp[13], SMALL_FASTF)
+	|| !NEAR_ZERO(matp[14], SMALL_FASTF))
+    {
 	status |= STAT_PERSP;
+    }
 
-    if (matp[15] != 1.0) status |= STAT_SCALE;
+    if (!NEAR_ZERO(matp[15], SMALL_FASTF))
+	status |= STAT_SCALE;
 
     return status;
 }
@@ -208,6 +217,7 @@ rt_comb_import4(
 
     BU_CK_EXTERNAL(ep);
     rp = (union record *)ep->ext_buf;
+    if (dbip) RT_CK_DBI(dbip);
 
     if (rp[0].u_id != ID_COMB) {
 	bu_log("rt_comb_import4: Attempt to import a non-combination\n");
@@ -276,9 +286,11 @@ rt_comb_import4(
 	    }
 
 	    /* Verify that perspective isn't used as a modeling transform */
-	    if (diskmat[12] != 0 || diskmat[13] != 0 || diskmat[14] != 0) {
-		bu_log("ERROR: %s/%s has perspective transform\n",
-		       rp[0].c.c_name, namebuf);
+	    if (!NEAR_ZERO(diskmat[12], SMALL_FASTF)
+		|| !NEAR_ZERO(diskmat[13], SMALL_FASTF)
+		|| !NEAR_ZERO(diskmat[14], SMALL_FASTF))
+	    {
+		bu_log("ERROR: %s/%s has perspective transform\n", rp[0].c.c_name, namebuf);
 	    }
 
 	    /* See if disk record is identity matrix */
@@ -408,7 +420,7 @@ int
 rt_comb_export4(
     struct bu_external *ep,
     const struct rt_db_internal *ip,
-    double local2mm,
+    double local2mm __attribute__((unused)),
     const struct db_i *dbip,
     struct resource *resp)
 {
@@ -423,6 +435,7 @@ rt_comb_export4(
     struct bu_vls tmp_vls;
 
     RT_CK_DB_INTERNAL(ip);
+    if (dbip) RT_CK_DBI(dbip);
     RT_CK_RESOURCE(resp);
     if (ip->idb_type != ID_COMBINATION) bu_bomb("rt_comb_export4() type not ID_COMBINATION");
     comb = (struct rt_comb_internal *)ip->idb_ptr;
@@ -533,7 +546,7 @@ rt_comb_export4(
     } else {
 	endp = strchr(bu_vls_addr(&tmp_vls), ' ');
 	if (endp) {
-	    int len;
+	    size_t len;
 	    len = endp - bu_vls_addr(&tmp_vls);
 	    if (len <= 0 && bu_vls_strlen(&tmp_vls) > 0) {
 		bu_log("WARNING: leading spaces on shader '%s' implies NULL shader\n",
@@ -555,7 +568,7 @@ rt_comb_export4(
 	    bu_strlcpy(rp[0].c.c_matname, bu_vls_addr(&tmp_vls), sizeof(rp[0].c.c_matname));
 	    bu_strlcpy(rp[0].c.c_matparm, endp+1, sizeof(rp[0].c.c_matparm));
 	} else {
-	    if (bu_vls_strlen(&tmp_vls) >= sizeof(rp[0].c.c_matname)) {
+	    if ((size_t)bu_vls_strlen(&tmp_vls) >= sizeof(rp[0].c.c_matname)) {
 		bu_log("ERROR:  Shader name '%s' exceeds v4 database field, aborting.\n",
 		       bu_vls_addr(&tmp_vls));
 		return -1;
@@ -901,12 +914,13 @@ rt_comb_describe(
     int verbose,
     double mm2local,
     struct resource *resp,
-    struct db_i *db_i)
+    struct db_i *dbip)
 {
     const struct rt_comb_internal *comb;
 
     RT_CK_DB_INTERNAL(ip);
     RT_CK_RESOURCE(resp);
+    if (dbip) RT_CK_DBI(dbip);
 
     comb = (struct rt_comb_internal *)ip->idb_ptr;
     RT_CK_COMB(comb);

src/librt/db_io.c
@@ -54,8 +54,7 @@ db_read(const struct db_i *dbip, genptr_t addr, long int count, long int offset)
     /* byte count */
     /* byte offset from start of file */
 {
-    register int got;
-    register long s;
+    int got;
 
     RT_CK_DBI(dbip);
     if (RT_G_DEBUG&DEBUG_DB) {

src/librt/db_lookup.c
@@ -485,8 +485,8 @@ db_pr_dir(register const struct db_i *dbip)
 void
 db_get_directory(register struct resource *resp)
 {
-    register struct directory	*dp;
-    register int		bytes;
+    struct directory	*dp;
+    size_t bytes;
 
     RT_CK_RESOURCE(resp);
     BU_CK_PTBL( &resp->re_directory_blocks );
@@ -494,7 +494,7 @@ db_get_directory(register struct resource *resp)
     BU_ASSERT_PTR( resp->re_directory_hd, ==, NULL );
 
     /* Get a BIG block */
-    bytes = bu_malloc_len_roundup(1024*sizeof(struct directory));
+    bytes = (size_t)bu_malloc_len_roundup(1024*sizeof(struct directory));
     dp = (struct directory *)bu_malloc(bytes, "re_directory_blocks from db_get_directory() " BU_FLSTR);
 
     /* Record storage for later */
@@ -569,8 +569,8 @@ db_lookup_by_attr(struct db_i *dbip, int dir_flags, struct bu_attribute_value_se
 	    
 	    draw = 0;
 	    match_count = 0;
-	    for ( i=0; i<avs->count; i++ ) {
-		for ( j=0; j<obj_avs.count; j++ ) {
+	    for ( i=0; (size_t)i<(size_t)avs->count; i++ ) {
+		for ( j=0; (size_t)j<(size_t)obj_avs.count; j++ ) {
 		    if ( !strcmp( avs->avp[i].name, obj_avs.avp[j].name ) ) {
 			if ( !strcmp( avs->avp[i].value, obj_avs.avp[j].value ) ) {
 			    if ( op == 2 ) {

src/librt/db_match.c
@@ -142,7 +142,11 @@ db_count_refs(struct db_i *dbip, struct rt_comb_internal *comb, union tree *comb
 {
     struct directory	*dp;
 
+    if (comb) RT_CK_COMB(comb);
     RT_CK_TREE( comb_leaf );
+    BU_ASSERT(dummy1 == NULL);
+    BU_ASSERT(dummy2 == NULL);
+    BU_ASSERT(dummy3 == NULL);
 
     if ( (dp=db_lookup(dbip, comb_leaf->tr_l.tl_name, LOOKUP_QUIET)) != DIR_NULL )
 	++dp->d_nref;

src/librt/db_open.c
@@ -44,10 +44,11 @@
 #include "raytrace.h"
 #include "db.h"
 #include "wdb.h"
+#include "mater.h"
 
 
 #ifndef SEEK_SET
-#  define SEEK_SET	0
+#  define SEEK_SET 0
 #endif
 
 #define DEFAULT_DB_TITLE "Untitled BRL-CAD Database"

src/librt/db_scan.c
@@ -35,6 +35,7 @@
 #include "vmath.h"
 #include "db.h"
 #include "raytrace.h"
+#include "mater.h"
 
 
 #define DEBUG_PR(aaa, rrr) 	{\
@@ -353,6 +354,7 @@ db_update_ident( struct db_i *dbip, const char *new_title, double local2mm )
     union record		rec;
     char			*old_title;
     int			v4units;
+    const char *ident = "/IDENT/";
 
     RT_CK_DBI(dbip);
 
@@ -374,7 +376,7 @@ db_update_ident( struct db_i *dbip, const char *new_title, double local2mm )
     if ( dbip->dbi_version > 4 )
 	return db5_update_ident( dbip, new_title, local2mm );
 
-    RT_DIR_SET_NAMEP(&dir, "/IDENT/");
+    RT_DIR_SET_NAMEP(&dir, ident);
     dir.d_addr = 0L;
     dir.d_len = 1;
     dir.d_magic = RT_DIR_MAGIC;

src/librt/db_tree.c
@@ -50,7 +50,7 @@ void
 db_dup_db_tree_state(struct db_tree_state *otsp, const struct db_tree_state *itsp)
 {
     int		shader_len=0;
-    int		i;
+    size_t i;
 
     RT_CK_DBTS(itsp);
     RT_CK_DBI(itsp->ts_dbip);
@@ -69,7 +69,7 @@ db_dup_db_tree_state(struct db_tree_state *otsp, const struct db_tree_state *its
 
     if ( itsp->ts_attrs.count > 0 ) {
 	bu_avs_init( &otsp->ts_attrs, itsp->ts_attrs.count, "otsp->ts_attrs" );
-	for ( i=0; i<itsp->ts_attrs.count; i++ )
+	for ( i=0; i<(size_t)itsp->ts_attrs.count; i++ )
 	    bu_avs_add( &otsp->ts_attrs, itsp->ts_attrs.avp[i].name,
 			itsp->ts_attrs.avp[i].value );
     } else {
@@ -175,7 +175,7 @@ db_free_combined_tree_state(register struct combined_tree_state *ctsp)
 void
 db_pr_tree_state(register const struct db_tree_state *tsp)
 {
-    int i;
+    size_t i;
 
     RT_CK_DBTS(tsp);
 
@@ -193,7 +193,7 @@ db_pr_tree_state(register const struct db_tree_state *tsp)
 	   tsp->ts_mater.ma_color[2] );
     bu_log(" ts_mater.ma_temperature=%g K\n", tsp->ts_mater.ma_temperature);
     bu_log(" ts_mater.ma_shader=%s\n", tsp->ts_mater.ma_shader ? tsp->ts_mater.ma_shader : "" );
-    for ( i=0; i<tsp->ts_attrs.count; i++ ) {
+    for ( i=0; i<(size_t)tsp->ts_attrs.count; i++ ) {
 	bu_log( "\t%s = %s\n", tsp->ts_attrs.avp[i].name, tsp->ts_attrs.avp[i].value );
     }
     bn_mat_print("ts_mat", tsp->ts_mat );
@@ -1111,7 +1111,6 @@ db_recurse(struct db_tree_state *tsp, struct db_full_path *pathp, struct combine
     struct directory	*dp;
     struct rt_db_internal	intern;
     union tree		*curtree = TREE_NULL;
-    static long int		depth = 0;
 
     RT_CK_DBTS(tsp);
     RT_CHECK_DBI( tsp->ts_dbip );
@@ -1960,7 +1959,8 @@ db_tally_subtree_regions(
 
 /* ============================== */
 
-HIDDEN union tree *db_gettree_region_end(register struct db_tree_state *tsp, struct db_full_path *pathp, union tree *curtree, genptr_t client_data)
+HIDDEN union tree *
+db_gettree_region_end(register struct db_tree_state *tsp, struct db_full_path *pathp, union tree *curtree, genptr_t client_data __attribute__((unused)))
 {
 
     RT_CK_DBTS(tsp);
@@ -1976,7 +1976,8 @@ HIDDEN union tree *db_gettree_region_end(register struct db_tree_state *tsp, str
     return(curtree);
 }
 
-HIDDEN union tree *db_gettree_leaf(struct db_tree_state *tsp, struct db_full_path *pathp, struct rt_db_internal *ip, genptr_t client_data)
+HIDDEN union tree *
+db_gettree_leaf(struct db_tree_state *tsp, struct db_full_path *pathp, struct rt_db_internal *ip, genptr_t client_data __attribute__((unused)))
 {
     register union tree	*curtree;
 

commit 160a6e1bd0a89df57877798c2a42c9ff08ff35fa

yet another example why strict compilation is a "good thing" (tm).  quell warning about old_max being set but unused.  turns out this was a bug introduced several years ago in r36723 after a simple refactoring.
git-svn-id: svn://svn.code.sf.net/p/brlcad/code/brlcad/trunk@45678 2f96ce8b-6d43-0410-b8df-bffccc660ffb

src/librt/prep.c
@@ -1819,7 +1819,7 @@ rt_reprep(struct rt_i *rtip, struct rt_reprep_obj_list *objs, struct resource *r
     bu_ptbl_free(&rtip->rti_new_solids);
 
     if (!VNEAR_EQUAL(rtip->mdl_min, old_min, SMALL_FASTF)
-	|| !VNEAR_EQUAL(rtip->mdl_max, old_min, SMALL_FASTF))
+	|| !VNEAR_EQUAL(rtip->mdl_max, old_max, SMALL_FASTF))
     {
 	/* fill out BSP, it must completely fill the model BB */
 	fastf_t bb[6];