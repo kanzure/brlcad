9062c7a277792805ca429bc4afd8db4229d814ed
starseeker <starseeker@2f96ce8b-6d43-0410-b8df-bffccc660ffb>
2011-10-07 22:19:37 +0000

    Add the full hv3 browser to src/other - preliminary for switching from special-purpose man and help viewers to customizations of the more powerful hv3 browser.  Prompted by the css additions to the Docbook output, but may also provide a text searching capability.
    git-svn-id: svn://svn.code.sf.net/p/brlcad/code/brlcad/trunk@47164 2f96ce8b-6d43-0410-b8df-bffccc660ffb

---
+++ src/other/hv3/hv3_doctype.tcl
@@ -0,0 +1,4 @@
+namespace eval hv3 { set {version($Id: hv3_doctype.tcl,v 1.9 2007/12/17 07:27:11 danielk1977 Exp $)} 1 }
+
+namespace eval hv3 {}
+
---
+++ src/other/hv3/main.tcl
@@ -0,0 +1,7 @@
+namespace eval hv3 {set {version($Id: main.tcl,v 1.7 2006/06/10 15:25:37 danielk1977 Exp $)} 1}
+
+package require starkit
+starkit::startup
+set ::HV3_STARKIT 1
+source [file join [file dirname [info script]] hv3_main.tcl] 
+
