cc849b8b87f4314feb3ca8f27abe137b90d6af6a
starseeker <starseeker@2f96ce8b-6d43-0410-b8df-bffccc660ffb>
2014-06-19 01:20:33 +0000

    Get rid of hv3 and sqlite - we're not currently using them, and svn has the history if we need it someday.
    git-svn-id: svn://svn.code.sf.net/p/brlcad/code/brlcad/trunk@61351 2f96ce8b-6d43-0410-b8df-bffccc660ffb

--- src/other/hv3/hv3_doctype.tcl
+++
@@ -1,4 +0,0 @@
-namespace eval hv3 { set {version($Id: hv3_doctype.tcl,v 1.9 2007/12/17 07:27:11 danielk1977 Exp $)} 1 }
-
-namespace eval hv3 {}
-
--- src/other/hv3/hv3_string.tcl
+++
@@ -1,3 +0,0 @@
-namespace eval hv3 { set {version($Id: hv3_string.tcl,v 1.9 2007/12/17 07:27:11 danielk1977 Exp $)} 1 }
-
-
